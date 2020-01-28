9bbbf5e7a5dba00aa3739127b27a54c4f8600cca
Eduardo Sanchez <edsanchez629@gmail.com>
2020-01-28 05:25:12 -0800

    updating files and js

---
+++ questions.js
@@ -0,0 +1,42 @@
+var questions = [
+    {
+        "question": "Web pages are designed using?",
+        "option1" : "HTML",
+        "option2": "CSS",
+        "option3": "Javascript",
+        "option4": "Jquery",
+        "answer": 2
+    }, 
+    {
+        "question": "Which CSS property changes background color?", 
+        "option1": "colour",
+        "option2": "bgColor",
+        "option3": "color",
+        "option4": "background-color",
+        "answer": 4
+    }, 
+    {
+        "question": "Which selector selects all the elements in CSS?", 
+        "option1": "*",
+        "option2": "+",
+        "option3": "$",
+        "option4": "#",
+        "answer": 1
+    }, 
+    {
+        "question": "What is the value of a in a+=b where a=10 & b=5?", 
+        "option1": "10",
+        "option2": "5",
+        "option3": "15",
+        "option4": "20",
+        "answer": 3
+    }, 
+    {
+        "question": "What does 'document' denote in a javascript statement?", 
+        "option1": "Javascript file",
+        "option2": "Css file",
+        "option3": "HTML page",
+        "option4": "XML document",
+        "answer": 3
+    }, 	
+    ];
